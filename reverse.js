6c5f52e9dadf7eb851f7981dbeb34d02bfd629e8
Nashil Damudi <nashildamudi21@gmail.com>
2025-09-13 20:45:01 +0530

    js loops

---
+++ reverse.js
@@ -0,0 +1,7 @@
+for (i = 5; i >=1; i--) {
+  line = "";
+  for (j = 1; j <= i; j++) {
+    line = line + "*";
+  }
+  console.log(line);
+}
---
+++ tables.js
@@ -0,0 +1,7 @@
+for (n = 1; n <= 10; n++) {
+    console.log("multiplicaton table"+ " " + n)
+  for (i = 1; i <= 10; i++) {
+    result = n * i;
+    console.log(result);
+  }
+}
